1629829ff61ee0c02ce36fde4a3d663061693562
Noi <noidor2019@gmail.com>
2024-02-03 23:06:16 +0700

    removed old implementation and moved all logic to life module

--- cmd/main.go
+++ cmd/main.go
@@ -1,33 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
 	life "github.com/Noiidor/go-game-of-life/life"
 )
 
 func main() {
-	field := [][]bool{
-		{false, false, false, false, false, false, false, false},
-		{false, false, false, false, false, false, false, false},
-		{false, false, false, true, true, false, false, false},
-		{false, false, true, false, true, false, false, false},
-		{false, false, false, false, true, false, false, false},
-		{false, false, false, false, false, false, false, false},
-		{false, false, false, false, false, false, false, false},
-		{false, false, false, false, false, false, false, false},
-	}
-
-	for {
-		//fmt.Printf("\033[0;0H")
-		fieldView := life.BuildFieldString(field)
-
-		fmt.Print(fieldView)
-
-		field = life.ProcessFieldIterationNew(field)
-
-		time.Sleep(time.Millisecond * 100)
-
-	}
+	life.StartGameOfLife(0, 100)
 }
--- life/life.go
+++ life/life.go
@@ -1,91 +1,47 @@
 package life
 
-import "strings"
-
-const (
-	deadCell = '—'
-	liveCell = '▮'
+import (
+	"strings"
+	"fmt"
+	"time"
 )
 
-func ProcessFieldIteration(field []string) []string {
-	var updatedField []string
-
-	for i := 0; i < len(field); i++ {
-		rowsCount := len(field)
-
-		currentLine := []rune(field[i])
-		previousLine := []rune(field[loopedIndex(i-1, rowsCount)])
-		nextLine := []rune(field[loopedIndex(i+1, rowsCount)])
-
-		updatedLine := make([]rune, len(currentLine))
-		copy(updatedLine, currentLine)
-
-		for j := 0; j < len(currentLine); j++ {
-
-			lineLength := len(currentLine)
-
-			isCellAlive := currentLine[j] == liveCell
-			liveNeighboursCount := 0
-
-			leftCell := currentLine[loopedIndex(j-1, lineLength)]
-			if leftCell == liveCell {
-				liveNeighboursCount++
-			}
-
-			rightCell := currentLine[loopedIndex(j+1, lineLength)]
-			if rightCell == liveCell {
-				liveNeighboursCount++
-			}
-
-			leftTopCell := previousLine[loopedIndex(j-1, lineLength)]
-			if leftTopCell == liveCell {
-				liveNeighboursCount++
-			}
-
-			centralTopCell := previousLine[loopedIndex(j, lineLength)]
-			if centralTopCell == liveCell {
-				liveNeighboursCount++
-			}
-
-			rightTopCell := previousLine[loopedIndex(j+1, lineLength)]
-			if rightTopCell == liveCell {
-				liveNeighboursCount++
-			}
-
-			leftBottomCell := nextLine[loopedIndex(j-1, lineLength)]
-			if leftBottomCell == liveCell {
-				liveNeighboursCount++
-			}
-
-			centralBottomCell := nextLine[loopedIndex(j, lineLength)]
-			if centralBottomCell == liveCell {
-				liveNeighboursCount++
-			}
-
-			rightBottomCell := nextLine[loopedIndex(j+1, lineLength)]
-			if rightBottomCell == liveCell {
-				liveNeighboursCount++
-			}
-
-			if isCellAlive {
-				if liveNeighboursCount > 3 || liveNeighboursCount < 2 {
-					updatedLine[j] = deadCell
-				}
-			} else {
-				if liveNeighboursCount == 3 {
-					updatedLine[j] = liveCell
-				}
-			}
-
-		}
-
-		updatedField = append(updatedField, string(updatedLine))
-
+func StartGameOfLife(iterations int, delayMs int){
+	field := [][]bool{
+		{false, false, false, false, false, false, false, false},
+		{false, false, false, false, false, false, false, false},
+		{false, false, false, true, true, false, false, false},
+		{false, false, true, false, true, false, false, false},
+		{false, false, false, false, true, false, false, false},
+		{false, false, false, false, false, false, false, false},
+		{false, false, false, false, false, false, false, false},
+		{false, false, false, false, false, false, false, false},
 	}
-	return updatedField
+
+	if iterations < 1 {
+		for {
+			fieldView := buildFieldString(field)
+			//fmt.Printf("\033[0;0H")
+			fmt.Print(fieldView)
+
+			field = processFieldIteration(field)
+
+			time.Sleep(time.Millisecond * time.Duration(delayMs))
+		}
+	}else{
+		for i := 0; i < iterations; i++ {
+			fieldView := buildFieldString(field)
+			//fmt.Printf("\033[0;0H")
+			fmt.Print(fieldView)
+
+			field = processFieldIteration(field)
+
+			time.Sleep(time.Millisecond * time.Duration(delayMs))
+		}
+	}
 }
 
-func ProcessFieldIterationNew(field [][]bool) [][]bool {
+func processFieldIteration(field [][]bool) [][]bool {
 
 	updatedField := copyFieldSlice(field)
 
@@ -140,7 +96,7 @@ func copyFieldSlice(field [][]bool) [][]bool {
 	return copiedField
 }
 
-func BuildFieldString(field [][]bool) string {
+func buildFieldString(field [][]bool) string {
 	var builder strings.Builder
 
 	for _, line := range field {
