898b52df943c9900644f5fde3341f50d50b380c2
Noi <noidor2019@gmail.com>
2024-02-05 02:27:41 +0700

    Fixed wrong digits overriding and updated README

--- cmd/main.go
+++ cmd/main.go
@@ -9,14 +9,14 @@ import (
 func main() {
 	numOfIterations := flag.Int("iter", 0, "Number of iterations to run. 0 for infinite loop.")
 
-	delay := flag.Int("delay", 100, "Delay between iterations(ms). Default is 100.")
+	delay := flag.Int("delay", 100, "Delay between iterations(ms).")
 
-	fieldHeight := flag.Int("height", 30, `Height of game field in cells. Default is 30. 
+	fieldHeight := flag.Int("height", 30, `Height of game field in cells.
 					In some terminals height or width that exceeds terminal borders will break game field display.`)
 
-	fieldWidth := flag.Int("width", 30, "Width of game field in cells. Default is 30.")
+	fieldWidth := flag.Int("width", 30, "Width of game field in cells.")
 
-	livePercentage := flag.Int("start-live", 35, "Percent of live cells in first iteration. Default is 35.")
+	livePercentage := flag.Int("start-live", 35, "Percent of live cells in first iteration.")
 
 	flag.Parse()
 
